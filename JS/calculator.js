e621e5e83329ea56a3d98a47676076174ce20eda
JayWolf1312 <jakob.wolfsebo@gmail.com>
2023-11-13 07:08:40 +0100

    added a few changes

---
+++ JS/calculator.js
@@ -0,0 +1,8 @@
+const Namen = ["Jakob", "Armend", "Daniel", "Phil"];
+for (let i = 0; i < 100000; i++){
+    const index = i % Namen.length;
+    console.log(Namen[index]);
+}
+
+
+const plainText = "This is my password";
