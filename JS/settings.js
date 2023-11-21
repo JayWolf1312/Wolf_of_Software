ad84862db759a403c5cdc7ea5c3793c35c214fff
JayWolf1312 <jakob.wolfsebo@gmail.com>
2023-11-21 06:58:00 +0100

    settings

--- JS/settings.js
+++ JS/settings.js
@@ -3,3 +3,9 @@ function changeFontSize(){
     const fontSize = document.addEventListener("Font-Size");
 
 }
+
+changeFontSize();
+
+const input = document.getElementById("input").value;
+input.textContent = input;
+
