d8288a923f9bfb5b4f1f01d7c9005755f33c6cd3
JayWolf1312 <jakob.wolfsebo@gmail.com>
2023-11-21 14:19:36 +0100

    setting

--- JS/settings.js
+++ JS/settings.js
@@ -1,5 +1,9 @@
 function toggleList(listID){
     const list = document.getElementById(listID);
     list.style.display = list.style.display === 'none' ? 'block' : 'none'; 
-    
+}
+
+function toggleUsername(usernameID){
+    const username = document.getElementById(usernameID);
+    username.style.display = username.style.display === 'none' ? 'block' : 'none';
 }
